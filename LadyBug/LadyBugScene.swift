54a99b438ec0a088b8539ef92d842e0c559a5cef
seo-kh <gospelseokh@gmail.com>
2023-01-24 01:59:03 +0900

    #1 [Feat] 랜덤한 블록생성

--- LadyBug/LadyBugScene.swift
+++ LadyBug/LadyBugScene.swift
@@ -14,13 +14,13 @@ import GameplayKit
 final class LadyBugScene: SKScene, SKPhysicsContactDelegate {
     
     private var ladyBug: SKSpriteNode?
-    private var block0: SKSpriteNode?
+    private var blocks: [SKSpriteNode]?
     private var lifes: [SKSpriteNode]?
     private var isContact = false
     
     override func sceneDidLoad() {
         ladyBug = childNode(withName: "ladyBug") as? SKSpriteNode
-        block0 = childNode(withName: "block0") as? SKSpriteNode
+        blocks = (0 ..< 5).map(generateBlock)
         lifes = self["life"] as? [SKSpriteNode]
 
         self.physicsWorld.contactDelegate = self
@@ -38,9 +38,6 @@ final class LadyBugScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
-    /// 1) life가 없으면 게임끝.
-    /// 2) 접촉이 있으면 ladyBug는 원점위치, life감소, contact여부 재설정
-    /// 3) 이외의 경우에는 ladyBug의 위치변경
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard !(lifes?.isEmpty ?? false) else { return }
         
@@ -62,5 +59,33 @@ final class LadyBugScene: SKScene, SKPhysicsContactDelegate {
 extension LadyBugScene {
     func didBegin(_ contact: SKPhysicsContact) {
         isContact = true
+    }
+    
+    /// block을 생성하는 함수
+    /// - Parameter element: 임의의 수
+    /// - Returns: 블록 타입
+    private func generateBlock<T>(_ element: T) -> SKSpriteNode {
+        let block = SKSpriteNode(imageNamed: "block")
+        block.setScale(0.3)
+        block.physicsBody = SKPhysicsBody(rectangleOf: block.size)
+        block.physicsBody?.categoryBitMask = 1
+        block.physicsBody?.affectedByGravity = false
+        block.physicsBody?.allowsRotation = false
+        block.physicsBody?.isDynamic = false
+        block.physicsBody?.pinned = false
+        block.position = CGPoint(x: .randomX, y: .randomY)
+        self.addChild(block)
+        return block
+    }
+}
+
+extension CGFloat {
+    /// block이 좌우를 넘지않으면서, ladyBug를 침범하지않는 최소 x 범위
+    static var randomX: CGFloat {
+        self.random(in: (-4.0 ..< -2.0)) * [-1.0, 1.0].randomElement()! * 64.0
+    }
+    /// block이 상하를 넘지않으면서, life를 넘지않으면서, ladyBug를 침범하지않는 최소 y 범위
+    static var randomY: CGFloat {
+        self.random(in: (-8.0 ..< -2.0)) * [-1.0, 1.0].randomElement()! * 64.0
     }
 }
