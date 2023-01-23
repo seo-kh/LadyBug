b9f6af9d3129238c15eaa426b6994019fd0719f6
seo-kh <gospelseokh@gmail.com>
2023-01-24 00:24:08 +0900

    #1 [Feat] Contact Physics 실험 성공

--- LadyBug/LadyBugScene.swift
+++ LadyBug/LadyBugScene.swift
@@ -8,29 +8,32 @@
 import SpriteKit
 import GameplayKit
 
-class LadyBugScene: SKScene {
+/// TODO: Contact Simulation!
+///
+/// link: [here](https://www.youtube.com/watch?v=43hzb4NmQfw)
+final class LadyBugScene: SKScene, SKPhysicsContactDelegate {
+    
     private var ladyBug: SKSpriteNode?
     private var block0: SKSpriteNode?
+    private var isContact = false
     
     override func sceneDidLoad() {
         ladyBug = childNode(withName: "ladyBug") as? SKSpriteNode
         block0 = childNode(withName: "block0") as? SKSpriteNode
+        self.physicsWorld.contactDelegate = self
         
-//        print("sceneDidLoad()!")
-//        print("ladyBug: ",ladyBug)
-//        print("block0: ",block0)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches {
             ladyBug?.position = t.location(in: self)
-//            print("bug position: ", ladyBug?.position)
         }
     }
+    
 }
 
 extension LadyBugScene {
     func didBegin(_ contact: SKPhysicsContact) {
-        print(contact.bodyA, contact.bodyB, contact.contactPoint)
+        print("contact! at ", contact.contactPoint)
     }
 }
